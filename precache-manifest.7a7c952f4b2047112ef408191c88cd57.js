053f8791c8b4200095d2859e690125599b063e0e
makarov-lesha <makarov.lesha@gmail.com>
2020-02-13 13:50:02 +0200

    Updates

---
+++ precache-manifest.7a7c952f4b2047112ef408191c88cd57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1ae873be71dda3562c6ebafa82a834f",
+    "url": "//first-deployed-app.io/index.html"
+  },
+  {
+    "revision": "7b5410b278613c4a5ba4",
+    "url": "//first-deployed-app.io/static/css/main.a9e25a4e.chunk.css"
+  },
+  {
+    "revision": "2074369d2e1b0123c9a5",
+    "url": "//first-deployed-app.io/static/js/2.c0344d36.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "//first-deployed-app.io/static/js/2.c0344d36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b5410b278613c4a5ba4",
+    "url": "//first-deployed-app.io/static/js/main.fb2db76b.chunk.js"
+  },
+  {
+    "revision": "acf5b44fa46928b83db8",
+    "url": "//first-deployed-app.io/static/js/runtime-main.c4c5df0e.js"
+  }
+]);
